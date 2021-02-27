7b81d9747c9a106e04fad65a77a2281b7029989f
Andrei Kolesnik <kolesnik.andrei@gmail.com>
2021-02-27 14:44:53 +0300

    Todos by Nodejs+Express+MongoDB

---
+++ fronted/NodeExpressMongoo/index.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const exphbs = require('express-handlebars')
+const todosRoutes = require('./routes/todos')
+
+
+
+const PORT =  process.env.PORT || 3000
+
+const app = express()
+const hbs = exphbs.create({
+    defaultLayout: 'main',
+    extname: 'hbs'
+})
+
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views', 'views')
+app.use(todosRoutes)
+
+
+async function start(){
+    try {
+        await mongoose.connect(
+            'mongodb+srv://andrei:andrei@cluster0.0hipu.mongodb.net/todos',
+            {
+            useNewUrlParser: true,
+            useFindAndModify: false
+        })
+        app.listen(PORT, () => {
+            console.log('Server started...')
+        })        
+    } catch (e) {
+        console.log(e)
+    }
+
+}
+
+
+
+start()
+
+
+
+
+
